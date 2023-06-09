b8013b4ffc5c4945bd1fc9c26cbfb93a72209b34
chen-wei <2201313713@qq.com>
2023-06-09 21:07:13 +0800

    git测试类

---
+++ src/main/java/test/GitTest.java
@@ -0,0 +1,14 @@
+package test;
+
+/**
+ * @author : chen wei
+ * @date : 2023/6/9 21:05
+ * @Version: 1.0
+ * @Description :
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
